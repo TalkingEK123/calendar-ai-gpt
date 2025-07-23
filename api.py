fed09c685eb969c01b88183423742673dc5f5272
Youssef Ismail <eyi1901@umoncton.ca>
2025-07-23 11:24:10 -0300

    Update api.py

--- api.py
+++ api.py
@@ -327,3 +327,9 @@ if __name__ == "__main__":
         os.remove("token.json")
 
     app.run()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
